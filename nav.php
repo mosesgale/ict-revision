9d34a67ce3336b44c89cb03e857dadbeed5bf994
Moses <mgale97+ghc@gmail.com>
2013-04-10 17:33:45 +0100

    OVERHAUL 2 part a

--- nav.php
+++ nav.php
@@ -6,28 +6,28 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</a>
-			<a class="brand" href="./index.php">AQA GCSE ICT Revision</a>
+			<a class="brand" href="./index.html">AQA GCSE ICT Revision</a>
 			<div class="nav-collapse collapse">
 				<ul class="nav">
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Sections <b class="caret"></b></a>
 						<ul class="dropdown-menu">
-							<li><a href="./section1.php">1. Computers and Hardware</a></li>
-							<li><a href="./section2.php">2. Operating Systems and Applications</a></li>
-							<li><a href="./section3.php">3. Spreadsheets and Databases</a></li>
-							<li><a href="./section4.php">4. Word Processing and Desktop Publishing</a></li>
-							<li><a href="./section5.php">5. Presentation software</a></li>
-							<li><a href="./section6.php">6. Measurement and Control</a></li>
-							<li><a href="./section7.php">7. Networks and Communication</a></li>
-							<li><a href="./section8.php">8. Making ICT Systems</a></li>
-							<li><a href="./section9.php">9. Processing Information</a></li>
-							<li><a href="./sectionx.php">10. Other ICT Issues</a></li>
+							<li><a href="../section1.html">1. Computers and Hardware</a></li>
+							<li><a href="../section2.html">2. Operating Systems and Applications</a></li>
+							<li><a href="../section3.html">3. Spreadsheets and Databases</a></li>
+							<li><a href="../section4.html">4. Word Processing and Desktop Publishing</a></li>
+							<li><a href="../section5.html">5. Presentation software</a></li>
+							<li><a href="../section6.html">6. Measurement and Control</a></li>
+							<li><a href="../section7.html">7. Networks and Communication</a></li>
+							<li><a href="../section8.html">8. Making ICT Systems</a></li>
+							<li><a href="../section9.html">9. Processing Information</a></li>
+							<li><a href="../sectionx.html">10. Other ICT Issues</a></li>
 							<li class="divider"></li>
 							<li class="nav-header">NOTE:</br>Each section relates to a section in the CGP revision guide</li>
 						</ul>
 					</li>
-					<li><a href="./past-papers.php">Past papers</a></li>
-					<li><a href="./more-revision.php">More revision</a></li>
+					<li><a href="../past-papers.html">Past papers</a></li>
+					<li><a href="../more-revision.html">More revision</a></li>
 				</ul>
 				<ul class="nav pull-right">
 					<li><a href="http://lp.rydeacademy.org/course/category.php?id=111" target="_blank" title="Go to the Ryde Academy Learning Platform">Learning Platform <i class="icon-share-alt"></i></a></li>
